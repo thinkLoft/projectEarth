e2d9b6f3143f982d36f43b3a4708a4897026791d
thinkLoft <kareem.lofters@gmail.com>
2018-11-06 20:41:54 -0500

    added firebase script to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -14,11 +14,87 @@ firebase.initializeApp(config);
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // user is signed in.
+    console.log(user);
   } else {
     // No user is signed in.
+    console.log("no user");
   }
 });
 
 $(document).ready(function() {
-  var db = firebase.database();
+  // ================================
+  // ======= Authentication =========
+  // ================================
+
+  // LOGIN LISTENER
+  $(document).on("click", "#login", function() {
+    email = $("#email")
+      .val()
+      .trim();
+    pwd = $("#password")
+      .val()
+      .trim();
+
+    // LOGIN FUNCTION
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, pwd)
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode === "auth/wrong-password") {
+          alert("Wrong password.");
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+  });
+
+  // LOGOUT LISTENER
+  $(document).on("click", "#logout", function() {
+    firebase
+      .auth()
+      .signOut()
+      .then(function() {
+        // Sign-out successful.
+        console.log("Sign out Successfull");
+      })
+      .catch(function(error) {
+        // An error happened.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        alert(errorMessage);
+      });
+  });
+
+  // SIGN UP  LISTENER
+  $(document).on("click", "#signUp", function() {
+    email = $("#email")
+      .val()
+      .trim();
+    pwd = $("#password")
+      .val()
+      .trim();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, pwd)
+      .then(function() {
+        alert(
+          "Account successfully created! You will be re-directed to the home page"
+        );
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        alert(errorMessage);
+        // ...
+      });
+  });
 });
