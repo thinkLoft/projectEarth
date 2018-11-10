90caa2a2a436f16929c96ece4918dac95ed365cd
thinkLoft <kareem.lofters@gmail.com>
2018-11-10 17:49:01 -0500

    working login and logout function and updated navigation...Firebase imlimented

---
+++ client/src/components/signin/signin.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { auth } from "../../firebase";
+import * as routes from "../../constants/routes";
+import "./signin.css";
+
+const SignInPage = ({ history }) => (
+  <div>
+    <SignInForm history={history} />
+  </div>
+);
+
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null
+};
+
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value
+});
+
+class SignInForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const { email, passwordOne } = this.state;
+
+    const { history } = this.props;
+
+    auth
+      .doSignInWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        this.setState({ ...INITIAL_STATE });
+        history.push(routes.HOME);
+      })
+      .catch(error => {
+        this.setState(byPropKey("error", error));
+      });
+  };
+
+  render() {
+    const { email, passwordOne, error } = this.state;
+
+    return (
+      <div id="loginPage" className="container">
+        <form id="login" onSubmit={this.onSubmit}>
+          <div id="loginForm" className="form-group">
+            <h1 id="loginHead">Login Page</h1>
+            <br />
+            <input
+              id="email"
+              className="form-control email"
+              value={email}
+              onChange={event =>
+                this.setState(byPropKey("email", event.target.value))
+              }
+              type="email"
+              placeholder="Email Address"
+            />
+            <br />
+            <input
+              id="password"
+              value={passwordOne}
+              className="form-control password"
+              onChange={event =>
+                this.setState(byPropKey("passwordOne", event.target.value))
+              }
+              type="password"
+              placeholder="Password"
+            />
+            <br />
+            <div id="buttons">
+              <button type="submit" className="btn btn-primary login">
+                Login
+              </button>{" "}
+            </div>
+            {error && <p>{error.message}</p>}
+          </div>
+        </form>
+        {SignUpLink()}
+      </div>
+    );
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={routes.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
+export default withRouter(SignInPage);
+
+export { SignInForm };
---
+++ client/src/components/signup/signup.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { auth } from "../../firebase";
+import * as routes from "../../constants/routes";
+import "./signup.css";
+
+const SignUpPage = ({ history }) => (
+  <div>
+    <SignUpForm history={history} />
+  </div>
+);
+
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null
+};
+
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value
+});
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = event => {
+    console.log("submit button pressed");
+    const { email, passwordOne, passwordTwo } = this.state;
+
+    const { history } = this.props;
+    if (passwordOne !== passwordTwo) {
+      const error = {
+        message: "passwords do not match"
+      };
+      this.setState(byPropKey("error", error));
+    } else {
+      auth
+        .doCreateUserWithEmailAndPassword(email, passwordOne)
+        .then(authUser => {
+          this.setState({ ...INITIAL_STATE });
+          console.log(authUser.email);
+          history.push(routes.HOME);
+        })
+        .catch(error => {
+          this.setState(byPropKey("error", error));
+        });
+    }
+    event.preventDefault();
+  };
+
+  render() {
+    const { email, passwordOne, passwordTwo, error } = this.state;
+
+    return (
+      <div id="loginPage" className="container">
+        <form id="login" onSubmit={this.onSubmit}>
+          <div id="loginForm" className="form-group">
+            <h1 id="loginHead">Sign Up Page</h1>
+            <br />
+            <input
+              id="email"
+              className="form-control email"
+              value={email}
+              onChange={event =>
+                this.setState(byPropKey("email", event.target.value))
+              }
+              type="email"
+              placeholder="Email Address"
+            />
+            <br />
+            <input
+              id="password"
+              value={passwordOne}
+              className="form-control password"
+              onChange={event =>
+                this.setState(byPropKey("passwordOne", event.target.value))
+              }
+              type="password"
+              placeholder="Password"
+            />
+            <br />
+            <input
+              className="form-control password"
+              value={passwordTwo}
+              onChange={event =>
+                this.setState(byPropKey("passwordTwo", event.target.value))
+              }
+              type="password"
+              placeholder="Confirm Password"
+            />
+            <br />
+            <div id="buttons">
+              <button
+                // disabled={isInvalid}
+                type="submit"
+                className="btn btn-primary signUp"
+              >
+                Sign Up
+              </button>{" "}
+            </div>
+            {error && <p>{error.message}</p>}
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={routes.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
+export default withRouter(SignUpPage);
+
+export { SignUpForm, SignUpLink };
