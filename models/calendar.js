243ad5da1b538e1e56da50dacfeb460cb23cfa86
Suraj Jagdev <suraj_57@hotmail.com>
2018-11-11 19:55:20 -0500

    made the initial mongoose schema, exported it, still need to make basic association with shared calender

---
+++ models/calendar.js
@@ -0,0 +1,32 @@
+//require in mongoose npm package
+import Mongoose from 'mongoose';
+//create a new schema model
+const Schema = Mongoose.Schema;
+//create calenderSchema
+let calenderSchema = new Schema({
+  //userEmail, intially commenting out required: true for testing
+  userEmail: {
+    type: String,
+    //required: true,
+    unique: true
+  },
+  //link for the article
+  Name: {
+    type: String
+    /*,
+    required: true*/
+  },
+  //personal Calendar
+  personalCalendar: [{ startDate: { type: Date }, endDate: { type: Date } }],
+
+  //association with other people for shared calenders
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'masterCalender'
+    }
+  ]
+});
+//create the article model with the articleSchema
+const myCalender = mongoose.model('myCalender', calenderSchema);
+export default myCalender;
