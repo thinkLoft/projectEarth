6ca7a1b72189dcc8697156a463471160c9605c54
Suraj Jagdev <suraj_57@hotmail.com>
2018-11-14 18:59:39 -0500

    requiring in and using routes file

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+//heroku has its own node environment, so only requiring in dotenv if not production
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+//require in express server
 const express = require('express');
+//require in mongoose for database storage
 const mongoose = require('mongoose');
 const app = express();
+//port listening
 const PORT = process.env.PORT || 3001;
+//static pathing
 const path = require('path');
+//backend mongoose routes
+const routes = require('./routes/index.js');
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -13,13 +20,16 @@ app.use(express.json());
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static('client/build'));
 // }
-
+//use routes file
+app.use(routes);
+//locally connect mongoose to mongo db under specified name; in production use heroku's addons
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/projectearth');
 // Send every request to the React app
 // Define any API routes before this runs
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
+//listen in on port
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
