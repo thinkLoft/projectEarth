54eb89d0e3a15512742c568da42ad4d53fb6c8ea
r.lim406 <r.lim406@hotmail.com>
2018-11-14 19:38:51 -0500

    added navbar in bootstrap format

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -10,21 +10,51 @@ const Navigation = ({ authUser }) => (
 );
 
 const NavigationAuth = () => (
-  <ul>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <Link to={routes.LANDING}>Free Time</Link>
-    <Link to={routes.HOME}>Home</Link>
-    <Link to={routes.ACCOUNT}>Account</Link>
-    <Link to={routes.AVAILABILITY}>Availability</Link>
-    <SignOutButton />
-  </ul>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav authorized">
+        <li class="nav-item active">
+          <Link to={routes.HOME}>Home<span class="sr-only">(current)</span></Link>
+        </li>
+        <li class="nav-item">
+          <Link to={routes.ACCOUNT}>Account</Link>
+        </li>
+        <li class="nav-item">
+          <Link to={routes.AVAILABILITY}>Availability</Link>
+        </li>
+        <SignOutButton />
+      </ul>
+
+    </div>
+  </nav>
 );
 
 const NavigationNonAuth = () => (
-  <ul >
-    <Link to={routes.LANDING}>Landing</Link>
-    <Link to={routes.SIGN_IN}>Login</Link>
-    <Link to={routes.SIGN_UP}>Sign Up</Link>
-  </ul>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <Link to={routes.LANDING}>Free Time</Link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <Link to={routes.LANDING}>Landing<span class="sr-only">(current)</span></Link>
+        </li>
+        <li class="nav-item">
+          <Link to={routes.SIGN_IN}>Login</Link>
+        </li>
+        <li class="nav-item">
+          <Link to={routes.SIGN_UP}>Sign Up</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 );
 
 export default Navigation;
