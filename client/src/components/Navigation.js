3428be70d5c3dedb1806d0a38919e56d7a4c97bc
r.lim406 <r.lim406@hotmail.com>
2018-11-14 22:35:11 -0500

    installed npms for icon

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -12,6 +12,7 @@ const Navigation = ({ authUser }) => (
 const NavigationAuth = () => (
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
     <Link to={routes.LANDING}>Free Time</Link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
