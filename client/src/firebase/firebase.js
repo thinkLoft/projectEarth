5e4c98179abda11c56840b520eef8df038b42129
Suraj Jagdev <suraj_57@hotmail.com>
2018-11-16 20:09:38 -0500

    firebase/fireabse.js

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,14 +1,14 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyDf64i8iNacWByhBUNZUKq5SQw7RguwtIg",
-  authDomain: "freetime-35743.firebaseapp.com",
-  databaseURL: "https://freetime-35743.firebaseio.com",
-  projectId: "freetime-35743",
-  storageBucket: "freetime-35743.appspot.com",
-  messagingSenderId: "297857687124"
+  apiKey: 'AIzaSyDf64i8iNacWByhBUNZUKq5SQw7RguwtIg',
+  authDomain: 'freetime-35743.firebaseapp.com',
+  databaseURL: 'https://freetime-35743.firebaseio.com',
+  projectId: 'freetime-35743',
+  storageBucket: 'freetime-35743.appspot.com',
+  messagingSenderId: '297857687124'
 };
 
 if (!firebase.apps.length) {
