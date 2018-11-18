925f617742d8170170147bbf36a0dcde8476c0ea
Suraj Jagdev <suraj_57@hotmail.com>
2018-11-18 15:53:25 -0500

    trying to undo single quote conflict error in PR

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
-import Navigation from './Navigation';
-import Login from './login/';
-import LandingPage from './Landing';
-import SignUpPage from './signup/';
-import SignInPage from './signin/';
-import PasswordForgetPage from './PasswordForget';
-import HomePage from './Home';
-import AccountPage from './Account';
-import { firebase } from '../firebase';
-import Availability from './Availability';
-import axios from 'axios';
-import API from '../utils/api.js';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Navigation from "./Navigation";
+import Login from :"./login/";
+import LandingPage from "./Landing";
+import SignUpPage from "./signup/";
+import SignInPage from "./signin/";
+import PasswordForgetPage from "./PasswordForget";
+import HomePage from "./Home";
+import AccountPage from "./Account";
+import { firebase } from "../firebase";
+import Availability from "./Availability";
+import axios from "axios";
+import API from "../utils/api.js";
 
-import * as routes from '../constants/routes';
+import * as routes from "../constants/routes";
 
 class App extends Component {
   constructor(props) {
@@ -27,32 +27,43 @@ class App extends Component {
   //show user's email, displayName, phoneNumber, and photoURL
   /*showUser = () => {
     if (this.state.authUser !== null && this.state.authUser !== 'undefined') {
-      console.log('uid : ' + this.state.authUser.uid);
+      const userUid = this.state.authUser.uid;
+      const usersEmail = this.state.authUser.email;
+      const userName = this.state.authUser.displayName;
+      console.log(userUid, usersEmail, userName);
+       console.log('uid : ' + this.state.authUser.uid);
       console.log(`The following is from showUser function\n
       Email: ${this.state.authUser.email}\n
       displayName: ${this.state.authUser.displayName}\n
       console.log('phoneNumber: ' + this.state.authUser.phoneNumber`);
       console.log('photoUrl : ' + this.state.authUser.photoURL);
+      this.saveUser(userUid, usersEmail, userName);
+      console.log('done');
     } else if (this.state.authUser === null) {
       console.log('no user is logged in');
     }
   };*/
-  /*
-  saveUser = event => {
-    event.preventDefault();
+
+  /*saveUser = (a, b, c) => {
     console.log('saving user');
     if (
       this.state.authUser !== null &&
       this.state.authUser !== 'undefined' &&
       this.state.authUser !== 'error'
     ) {
+      console.log('a, b, c: ' + a, b, c);
       API.newUser({
-        userEmail: this.state.authUser.email,
-        name: this.state.authUser.displayName
-      }).then(res => {
-        console.log('Success');
-        console.log(res);
-      });
+        uid: a,
+        userEmail: b,
+        name: c
+      })
+        .then(res => {
+          console.log('Success');
+          console.log(res);
+        })
+        .catch(error => {
+          console.log('saveUser(): ' + error);
+        });
     }
   };*/
 
