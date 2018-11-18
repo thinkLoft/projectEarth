abee98575980f26a4a47c00c12c517a5d3f847e8
Suraj Jagdev <suraj_57@hotmail.com>
2018-11-18 16:11:22 -0500

    single quotes to double quotes

--- routes/personalCalender/routes.js
+++ routes/personalCalender/routes.js
@@ -1,20 +1,20 @@
 //require in router
-const router = require('express').Router();
-const personalCalender = require('../../controllers/controller.js');
+const router = require("express").Router();
+const personalCalender = require("../../controllers/controller.js");
 
 //hit backend route /
 //we post a new users information and save into db
 //for testing purposes findall all users
 router
-  .route('/')
+  .route("/")
   .post(personalCalender.newUser)
   .get(personalCalender.findEverything);
 //find a user by their id
 //update a users personalCalender's startDate and endDate
-router.route('/:id').get(personalCalender.findById);
+router.route("/:id").get(personalCalender.findById);
 //finds by uid and posts to the db for new dates
 router
-  .route('/uid/:uid')
+  .route("/uid/:uid")
   .get(personalCalender.findByUid)
   .post(personalCalender.update);
 //export the router
