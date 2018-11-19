7d5bdae202d461f7991c3f43d5a01ae5db8d851f
Suraj Jagdev <suraj_57@hotmail.com>
2018-11-18 19:44:18 -0500

    resolving diff

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Account.css";
-// import UserCalendar from "../UserCalendar";
+import UserCalendar from "../UserCalendar";
 import Form from "../Form";
 import { firebase } from "../../firebase";
 
