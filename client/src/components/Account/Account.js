3dd8292f627644324efd2d138f4ce5b7f88fc555
Suraj Jagdev <suraj_57@hotmail.com>
2018-11-20 18:19:56 -0500

    saving updating user

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -3,20 +3,43 @@ import './Account.css';
 import UserCalendar from '../UserCalendar';
 import Form from '../Form';
 import { firebase } from '../../firebase';
+//importing API calls
+import API from '../../utils/api.js';
 
 let authUser;
-let authUid;
+let authUid = '';
 firebase.auth.onAuthStateChanged(user => {
   if (user !== null) {
     authUser = user.email;
     authUid = user.uid;
-    console.log(user);
+    //console.log(user);
     console.log(authUser, authUid);
+    //find user by uid, if user does not exisit make a user
+    API.findUid(authUid).then(data => {
+      console.log('hit api authuid @ account js line 19');
+      console.log(data);
+      if (!data.length) {
+        console.log('if not data.length uid: ', authUid);
+        API.newUser(authUid, authUser)
+          .then(dataInformation => {
+            console.log(
+              'Success hit api new user @ line 23 ' + dataInformation
+            );
+          })
+          .catch(error => {
+            if (error) {
+              console.log(error);
+            }
+          });
+      }
+    });
+    //
   } else {
     // No user is signed in.
     console.log('user login status: There is no logged in user');
   }
 });
+
 //sending props user email and uid
 const AccountPage = ({ authUser }) => (
   <div id="addTime" className="container">
