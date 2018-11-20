bae8e8349fd6616f15d7bd7daeeb16ddd4e28485
Brandon Weir <brandon.weir@alumni.utoronto.ca>
2018-11-20 09:12:44 -0500

    fullcalendar installed

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,27 +3,34 @@ import { Link } from "react-router-dom";
 
 import SignOutButton from "./SignOut";
 import * as routes from "../constants/routes";
-import './Navigation.css';
-import icon from './images/icon.jpg'
-
+import "./Navigation.css";
+import icon from "./images/icon.jpg";
 
 const Navigation = ({ authUser }) => (
-
   <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
 );
 
 const NavigationAuth = () => (
-
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <img class="calendarIcon" src={icon} alt="calendarIcon" />
     <Link to={routes.LANDING}>Free Time</Link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav authorized">
         <li class="nav-item active">
-          <Link to={routes.HOME}>Home<span class="sr-only">(current)</span></Link>
+          <Link to={routes.HOME}>
+            Home<span class="sr-only">(current)</span>
+          </Link>
         </li>
         <li class="nav-item">
           <Link to={routes.ACCOUNT}>Account</Link>
@@ -33,23 +40,31 @@ const NavigationAuth = () => (
         </li>
         <SignOutButton />
       </ul>
-
     </div>
   </nav>
 );
 
 const NavigationNonAuth = () => (
-
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <img class="calendarIcon" src={icon} alt="calendarIcon" />
     <Link to={routes.LANDING}>Free Time</Link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <Link to={routes.LANDING}>Landing<span class="sr-only">(current)</span></Link>
+          <Link to={routes.LANDING}>
+            Home<span class="sr-only">(current)</span>
+          </Link>
         </li>
         <li class="nav-item">
           <Link to={routes.SIGN_IN}>Login</Link>
