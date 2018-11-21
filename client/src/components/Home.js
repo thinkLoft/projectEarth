94c5d7a89c18656f130911d80e1745446f93d1ce
r.lim406 <r.lim406@hotmail.com>
2018-11-20 20:59:54 -0500

    populated the cards and added the overlay and hover features

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,42 +1,52 @@
 import React from "react";
 import './Home.css';
 import availability from './images/availability.png'
+import availabilityInput from "./images/availabilityInput.png"
+import mutual from "./images/mutual.jpg"
 
 const HomePage = ({ authUser }) => (
   <div id="home">
     <div class="row explanation">
       <div class="col">
         <div class="card">
-          <img class="card-img-top" src={availability} alt="Card cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="text">I'm free during the following times...</div>
+          <div class="overlay">
+            <img class="card-img-top" width="200px" height="300px" src={availabilityInput} alt="Card cap" />
+            <div class="card-body">
+              <h5 class="card-title">Schedule your Free Time</h5>
+              <p class="card-text">Prep for future meetings or your social calendar by logging in and inputting when you're available.</p>
+            </div>
           </div>
         </div>
       </div>
 
       <div class="col">
         <div class="card">
-          <img class="card-img-top" src={availability} alt="Card cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="text">Let me make sure...</div>
+          <div class="overlay">
+            <img class="card-img-top" src={availability} alt="Card cap" />
+            <div class="card-body">
+              <h5 class="card-title">View your availability</h5>
+              <p class="card-text">See the layout of your day in detail as you input your free time.</p>
+            </div>
           </div>
         </div>
       </div>
 
       <div class="col">
         <div class="card">
-          <img class="card-img-top" src={availability} alt="Card cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="text">So...when are you free?</div>
+          <div class="overlay">
+            <img class="card-img-top" width="200px" height="250px" src={mutual} alt="Card cap" />
+            <div class="card-body">
+              <h5 class="card-title">Mutual Availability</h5>
+              <p class="card-text">Take the pain out of scheduling meetings. Free time will show you mutual availability based on everyone's inputs. Just choose an option and you're good to go!</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-
 );
 
 export default HomePage;
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,30 +7,39 @@ const LandingPage = () => (
     <div class="row explanation">
       <div class="col">
         <div class="card">
-          <img class="card-img-top" src={availability} alt="Card cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="text">I'm free during the following times...</div>
+          <div class="overlay">
+            <img class="card-img-top" src={availability} alt="Card cap" />
+            <div class="card-body">
+              <h5 class="card-title">Schedule your Free Time</h5>
+              <p class="card-text">Prep for future meetings or your social calendar by logging in and inputting when you're available.</p>
+            </div>
           </div>
         </div>
       </div>
 
       <div class="col">
         <div class="card">
-          <img class="card-img-top" src={availability} alt="Card cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="text">Let me make sure...</div>
+          <div class="overlay">
+            <img class="card-img-top" src={availability} alt="Card cap" />
+            <div class="card-body">
+              <h5 class="card-title">View your availability</h5>
+              <p class="card-text">See the layout of your day in detail as you input your free time.</p>
+            </div>
           </div>
         </div>
       </div>
 
       <div class="col">
         <div class="card">
-          <img class="card-img-top" src={availability} alt="Card cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="text">So...when are you free?</div>
+          <div class="overlay">
+            <img class="card-img-top" src={availability} alt="Card cap" />
+            <div class="card-body">
+              <h5 class="card-title">Mutual Availability</h5>
+              <p class="card-text">Take the pain out of scheduling meetings. Free time will show you mutual availability based on everyone's inputs. Just choose an option and you're good to go!</p>
+            </div>
           </div>
         </div>
       </div>
