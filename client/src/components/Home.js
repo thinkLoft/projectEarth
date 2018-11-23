159cfa52d3ec050131528a9d2c8c42c8af21ab37
r.lim406 <r.lim406@hotmail.com>
2018-11-22 19:24:25 -0500

    changed class to className

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,41 +6,41 @@ import mutual from "./images/mutual.jpg"
 
 const HomePage = ({ authUser }) => (
   <div id="home">
-    <div class="row explanation">
-      <div class="col">
-        <div class="card">
-          <div class="text">I'm free during the following times...</div>
-          <div class="overlay">
-            <img class="card-img-top" width="200px" height="300px" src={availabilityInput} alt="Card cap" />
-            <div class="card-body">
-              <h5 class="card-title">Schedule your Free Time</h5>
-              <p class="card-text">Prep for future meetings or your social calendar by logging in and inputting when you're available.</p>
+    <div className="row explanation">
+      <div className="col">
+        <div className="card">
+          <div className="text">I'm free during the following times...</div>
+          <div className="overlay">
+            <img className="card-img-top" width="200px" height="300px" src={availabilityInput} alt="Card cap" />
+            <div className="card-body">
+              <h5 className="card-title">Schedule your Free Time</h5>
+              <p className="card-text">Prep for future meetings or your social calendar by logging in and inputting when you're available.</p>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="col">
-        <div class="card">
-          <div class="text">Let me make sure...</div>
-          <div class="overlay">
-            <img class="card-img-top" src={availability} alt="Card cap" />
-            <div class="card-body">
-              <h5 class="card-title">View your availability</h5>
-              <p class="card-text">See the layout of your day in detail as you input your free time.</p>
+      <div className="col">
+        <div className="card">
+          <div className="text">Let me make sure...</div>
+          <div className="overlay">
+            <img className="card-img-top" src={availability} alt="Card cap" />
+            <div className="card-body">
+              <h5 className="card-title">View your availability</h5>
+              <p className="card-text">See the layout of your day in detail as you input your free time.</p>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="col">
-        <div class="card">
-          <div class="text">So...when are you free?</div>
-          <div class="overlay">
-            <img class="card-img-top" width="200px" height="250px" src={mutual} alt="Card cap" />
-            <div class="card-body">
-              <h5 class="card-title">Mutual Availability</h5>
-              <p class="card-text">Take the pain out of scheduling meetings. Free time will show you mutual availability based on everyone's inputs. Just choose an option and you're good to go!</p>
+      <div className="col">
+        <div className="card">
+          <div className="text">So...when are you free?</div>
+          <div className="overlay">
+            <img className="card-img-top" width="200px" height="250px" src={mutual} alt="Card cap" />
+            <div className="card-body">
+              <h5 className="card-title">Mutual Availability</h5>
+              <p className="card-text">Take the pain out of scheduling meetings. Free time will show you mutual availability based on everyone's inputs. Just choose an option and you're good to go!</p>
             </div>
           </div>
         </div>
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -14,21 +14,21 @@ const Navigation = ({ authUser }) => (
 
 const NavigationAuth = () => (
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <img class="calendarIcon" src={icon} alt="calendarIcon" />
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <img className="calendarIcon" src={icon} alt="calendarIcon" />
     <Link to={routes.LANDING}>Free Time</Link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav authorized">
-        <li class="nav-item active">
-          <Link to={routes.HOME}>Home<span class="sr-only">(current)</span></Link>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav authorized">
+        <li className="nav-item active">
+          <Link to={routes.HOME}>Home<span className="sr-only">(current)</span></Link>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
           <Link to={routes.ACCOUNT}>Account</Link>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
           <Link to={routes.AVAILABILITY}>Availability</Link>
         </li>
         <SignOutButton />
@@ -40,21 +40,21 @@ const NavigationAuth = () => (
 
 const NavigationNonAuth = () => (
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <img class="calendarIcon" src={icon} alt="calendarIcon" />
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <img className="calendarIcon" src={icon} alt="calendarIcon" />
     <Link to={routes.LANDING}>Free Time</Link>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav unauthorized">
-        <li class="nav-item active">
-          <Link to={routes.LANDING}>Landing<span class="sr-only">(current)</span></Link>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav unauthorized">
+        <li className="nav-item active">
+          <Link to={routes.LANDING}>Landing<span className="sr-only">(current)</span></Link>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
           <Link to={routes.SIGN_IN}>Login</Link>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
           <Link to={routes.SIGN_UP}>Sign Up</Link>
         </li>
       </ul>
