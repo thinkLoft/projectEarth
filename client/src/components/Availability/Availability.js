d7081ff49fb38a7fa8225c4a2d38daad04a1fd44
thinkLoft <kareem.lofters@gmail.com>
2018-11-27 20:53:59 -0500

    initial commit

--- client/src/components/Availability/Availability.js
+++ client/src/components/Availability/Availability.js
@@ -5,10 +5,8 @@ import "fullcalendar/dist/fullcalendar.js";
 // import "fullcalendar/dist/fullcalendar.print.min.css";
 import "./Availability.css";
 
-import ReactDOM from "react-dom";
 import FullCalendar from "fullcalendar-reactwrapper";
 
-import $ from "jquery";
 import "moment/min/moment.min.js";
 
 const AvailabilityPage = () => (
--- client/src/components/formtodaylist/list.js
+++ client/src/components/formtodaylist/list.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import moment from 'moment';
+import React from "react";
+import moment from "moment";
 const List = props => {
-  console.log(props.value);
-  let formatDate = moment(props.value).format('YYYY/MM/DD');
+  let formatDate = moment(props.value).format("YYYY/MM/DD");
   return (
     <div>
       <h3>{formatDate}</h3>
