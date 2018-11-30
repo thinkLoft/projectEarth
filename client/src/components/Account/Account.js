f5a67678eb0395bc88d8f1154c6b8119da08920b
Suraj Jagdev <suraj_57@hotmail.com>
2018-11-30 18:04:56 -0500

    local fix

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './Account.css';
 import Form from '../Form';
-<<<<<<< HEAD
 import ToDoForm from '../toDoForm';
-=======
->>>>>>> master
 
 //sending props user email and uid
 const AccountPage = ({ authUser }) => (
