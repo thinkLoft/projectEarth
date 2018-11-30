47f5654b166bd69e59b09eb0cea9006dfaba68ca
Brandon Weir <brandon.weir@alumni.utoronto.ca>
2018-11-29 20:21:48 -0500

    conflict fix

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Account.css";
 import Form from "../Form";
-import ToDoForm from "../ToDoForm";
+import toDoForm from "../toDoForm";
 
 //sending props user email and uid
 const AccountPage = ({ authUser }) => (
   <div id="addTime" className="container">
     <Form email={authUser.email} uid={authUser.uid} />
-    <ToDoForm email={authUser.email} uid={authUser.uid} />
+    <toDoForm email={authUser.email} uid={authUser.uid} />
   </div>
 );
 
